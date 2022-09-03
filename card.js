59b1b2df2a6cba75e8ca4b578ea60049baa7154d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-09-04 01:23:10 +0600

    added dynamic picture, title, details

--- card.js
+++ card.js
@@ -15,15 +15,15 @@ const displayCard = cards=>
        
        <div class="row g-0">
        <div class="col-md-4">
-      <img src="${card.img}" class="img-fluid rounded-start" alt="...">
+      <img src="${card.image_url}" class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">${card.title}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text">${card.details}</p>
           <h5 class ="card-writer">${card.author.name}</h5>
           <h6 class = "card-view">${card.total_view}</h6>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <p class="card-text"><small class="text-muted">${card.author.published_date}</small></p>
         </div>
       </div>
         
