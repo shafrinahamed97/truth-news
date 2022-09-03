17478e00ca938196de8a6cfeecdf3779d38b3560
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-09-04 01:17:28 +0600

    Start news card dynamic

--- card.js
+++ card.js
@@ -12,14 +12,23 @@ const displayCard = cards=>
         const cardDiv = document.createElement('div');
         cardDiv.classList.add('row');
         cardDiv.innerHTML = `
-       <div class ="card ">
-       <img src="..." class="card-img-top" alt="...">
-       <div class = "card-body>
-        <h5 class ="card-title">Card Title</h5>
-        <p class = "card-text">This is card text</p>
-        
+       
+       <div class="row g-0">
+       <div class="col-md-4">
+      <img src="${card.img}" class="img-fluid rounded-start" alt="...">
         </div>
-        <div>
+        <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${card.title}</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class ="card-writer">${card.author.name}</h5>
+          <h6 class = "card-view">${card.total_view}</h6>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+        
+        
+        
 
         `;
         cardContainer.appendChild(cardDiv);
