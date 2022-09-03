cc0b12f9d698388d4287eb86caba11cd64399554
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-09-03 20:08:41 +0600

    Finished menu section

---
+++ index.js
@@ -0,0 +1,34 @@
+const loadNews = () =>
+{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data =>displayNews(data.data.news_category))
+}
+
+
+const displayNews= (categories)=>{
+    const categoriesContainer  = document.getElementById('all-menu');
+    
+    categories.forEach(category => {
+        console.log(category)
+        console.log(category.category_name)
+
+    
+        const newli = document.createElement('li')
+        newli.innerHTML = `
+        <button> 
+              ${category.category_name}
+              
+        </button>
+        
+        `;
+        categoriesContainer.appendChild(newli);
+
+    })
+        
+    
+}
+loadNews();
+
+
+
